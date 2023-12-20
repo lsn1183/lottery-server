c1550ed3981017fc49db0de89fe8d36124b52569
lishengning <lsn1183@outlook.com>
2023-12-20 14:27:29 +0800

    12-20 code

--- src/common/config/env.development.ts
+++ src/common/config/env.development.ts
@@ -11,7 +11,7 @@ export default {
     host: 'localhost', // 服务器主机名  本地不用改 或者 127.0.0.1
     port: 3306, // 数据库端口  默认 3306
     username: 'root', // 数据库用户名
-    password: '12345678', // 数据库密码
+    password: 'az12345678', // 数据库密码
     database: 'database', // 数据库名 存储库
     // 注册 数据库实体类 如 photo/photo.entity.ts
     // 也可以这样写 "entities": ["dist/**/*.entity{.ts,.js}"],  匹配所有 .entity 文件，
